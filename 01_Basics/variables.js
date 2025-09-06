9ab79a5fffcf3c116e0a272e66191be6b42659bc
CH Y V S SRINIVAS <129578705+14yashwanth@users.noreply.github.com>
2025-09-06 13:36:23 +0000

    variables

---
+++ 01_Basics/variables.js
@@ -0,0 +1,11 @@
+const accountId = 1409
+let accountEmail = "yyashwanth109@gmail.com"
+var accountPassword = "131409"
+accountCity = "Hyderabad"
+let accountState;
+
+console.log(accountId);
+accountEmail = "yashwanth1409@gmail.com"
+accountPassword = "13140911"
+accountCity = "Benguluru"
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
