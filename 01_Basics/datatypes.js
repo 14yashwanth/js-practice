aaf0f67990f1a710b14bb15cfcfc3e5b075af5a0
CH Y V S SRINIVAS <129578705+14yashwanth@users.noreply.github.com>
2025-09-06 14:03:02 +0000

    data types

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,10 @@
+"use strict"; // treats all JS code as newer version
+let name = "Yashwanth" //string
+let age= 22 //number
+let isLoggedin = true //boolean
+// null => standalone value
+// undefined => value not asssigned
+// symbol => to find uniqueness
+// object
+
+console.log(typeof null); // null is a object
