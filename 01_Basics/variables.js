eed4f2bebfda626ade5aeadc9b900187d59729f5
CH Y V S SRINIVAS <129578705+14yashwanth@users.noreply.github.com>
2025-09-06 13:41:43 +0000

    added a note

--- 01_Basics/variables.js
+++ 01_Basics/variables.js
@@ -3,7 +3,9 @@ let accountEmail = "yyashwanth109@gmail.com"
 var accountPassword = "131409"
 accountCity = "Hyderabad"
 let accountState;
-
+/*
+Do not prefer to use var because of issue in block & functional scope
+*/
 console.log(accountId);
 accountEmail = "yashwanth1409@gmail.com"
 accountPassword = "13140911"
