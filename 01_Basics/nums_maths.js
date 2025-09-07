336b645656a4c3db88dc61477a0402457e57c3f9
CH Y V S SRINIVAS <129578705+14yashwanth@users.noreply.github.com>
2025-09-07 13:15:36 +0000

    Math & num

---
+++ 01_Basics/nums_maths.js
@@ -0,0 +1,31 @@
+const score = 400
+const balance = new Number(100)//to specify the datatype particularly
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(balance.toFixed(1));//100.0
+// console.log(balance.toPrecision(3));
+//================================================
+// console.log(math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.3));
+// console.log(Math.min(4,3,4,5,6,2));
+// console.log(Math.max(4,3,4,5,6,2));
+// console.log(Math.random());
+
+const min=10
+const max=20
+console.log(Math.floor((Math.random()*(max-min+1))+min));
+
+
+
+
+
+
+
+
+
+
+
+
