6baf0e094fe1b455c53605f69cdcc9eb1d305dc9
CH Y V S SRINIVAS <129578705+14yashwanth@users.noreply.github.com>
2025-09-07 09:25:22 +0000

    String methods

---
+++ 01_Basics/strings.js
@@ -0,0 +1,27 @@
+const name ="Yashwanth"
+const repocount = 50
+//console.log(`hello my name is ${name}`);
+const newgame= new String('Yashwan-th')//another way of declaring string
+// console.log(newgame[0]);
+// console.log(newgame.length);
+// console.log(newgame.toUpperCase());
+// console.log(newgame.charAt(4));
+// console.log(newgame.indexOf('t'));
+//console.log(newgame.split('-'));
+const newstring = newgame.substring(0,4)
+const anotherstring = newgame.slice(-8,4)
+//console.log(newstring);
+//console.log(anotherstring);
+const newstring1 = "   hitesh   "
+//console.log(newstring1.trim());
+const url ="https://yashwanth.com/"
+//console.log(url.replace('https', 'www'));
+
+
+
+
+
+
+
+
+
