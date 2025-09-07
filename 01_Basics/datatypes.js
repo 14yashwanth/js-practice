0dae6752e67c7f5ce793b4840379b8f82ec62076
CH Y V S SRINIVAS <129578705+14yashwanth@users.noreply.github.com>
2025-09-07 08:59:39 +0000

    stack and heap memory

--- 01_Basics/datatypes.js
+++ 01_Basics/datatypes.js
@@ -12,3 +12,6 @@ console.log(typeof null); // null is a object
 //Based on storing data and accessing data from memory data types are divided into 2 types!
 //primitive data types(call by value) => string, number, boolean, null(empty), undefined, symbol, bigInt
 //Non primitive data types(call by reference) => arrays, objects, functions
+
+//===================================================
+//Stack(Primitve) memory & Heap(Non-Primitive) memory
